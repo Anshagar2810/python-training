afcb220192a4eb08ce40d360dbf16c57e3c9182a
Anshagar2810 <161422941+Anshagar2810@users.noreply.github.com>
2026-01-21 09:56:05 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,17 @@
+#a,b=10,20   
+
+#print("sum of {a} and {b} is:{a+b}".format(a,b,a+b))
+
+#name = "anshcoder"
+#print(name[5::-1])
+
+
+#l1 = input("Enter a string: ")
+#if len(l1) != len(set(l1)):
+# print(True)
+#else:
+#    print(False)
+
+
+t1=(10)
+print (type(t1))
