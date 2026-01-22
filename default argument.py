b8eafb544c44b8dace3439d9103f2220b8afcd31
Anshagar2810 <161422941+Anshagar2810@users.noreply.github.com>
2026-01-22 10:17:50 +0530

    Add files via upload

---
+++ default argument.py
@@ -0,0 +1,15 @@
+def add(a, b=0, c=0, d=0):
+    return a + b + c + d
+
+# values given directly in code
+result1 = add(5)
+result2 = add(5, 10)
+result3 = add(5, 10, 15)
+result4 = add(5, 10, 15, 20)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
+#isme agar value nhi hogi b c ya d ki to 0 lelega warna jo value hogi wo lelega
