d8f65e94531a91236c3a8ff02e97b23686205ea7
Anshagar2810 <161422941+Anshagar2810@users.noreply.github.com>
2026-01-22 10:21:30 +0530

    Add files via upload

---
+++ tuple add.py
@@ -0,0 +1,15 @@
+def add(*t):
+    return sum(t)
+
+# values given directly in code
+result1 = add(5)
+result2 = add(5, 10)
+result3 = add(5, 10, 15)
+result4 = add(5, 10, 15, 20)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
+#no limitation jitni value doge wo krdega no need to assign
