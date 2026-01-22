b8690f51a051a1c30cde99423d6c1438d02a6474
Anshagar2810 <161422941+Anshagar2810@users.noreply.github.com>
2026-01-22 10:29:27 +0530

    Add files via upload

---
+++ binarysearch.py
@@ -0,0 +1,23 @@
+def search(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+
+# testing in VS Code
+nums = [-1, 0, 3, 5, 9, 12]
+target = 9
+
+result = search(nums, target)
+print("Index:", result)
