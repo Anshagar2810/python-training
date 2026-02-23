362c1b831109686dd6c1dd29aa191fe9d0fabb0b
Anshagar2810 <161422941+Anshagar2810@users.noreply.github.com>
2026-02-23 15:52:34 +0530

    Update reorderlist.py

--- reorderlist.py
+++ reorderlist.py
@@ -1,37 +1,45 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-
-        if not head or not head.next:
-            return
-
-        # Step 1: Find middle
-        slow = fast = head
-
-        while fast.next and fast.next.next:
-            slow = slow.next
-            fast = fast.next.next
-
-        # Step 2: Reverse second half
-        prev = None
-        curr = slow.next
-        slow.next = None
-
-        while curr:
-            nextNode = curr.next
-            curr.next = prev
-            prev = curr
-            curr = nextNode
-
-        # Step 3: Merge both halves
-        first = head
-        second = prev
-
-        while second:
-            temp1 = first.next
-            temp2 = second.next
-
-            first.next = second
-            second.next = temp1
-
-            first = temp1
-            second = temp2
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if head.next==None or head.next.next==None:
+            pass
+        else:
+            sp=fp=head
+            while fp.next and fp.next.next:
+                sp=sp.next
+                fp=fp.next.next
+            temp=sp.next
+            sp.next=None
+            start = None
+            while temp:
+                tm = temp
+                temp=temp.next
+                tm.next = start
+                start=tm
+               
+            temp=start
+            start=end=head
+            head=head.next
+            end.next=None
+            while head and temp:
+                tm=temp
+                temp=temp.next
+                tm.next=None
+                end.next=tm
+                end=tm
+                tm=head
+                head=head.next
+                tm.next=None
+                end.next=tm
+                end=tm
+                if temp!=None:
+                    end.next=temp
+                else:
+                    head=start
